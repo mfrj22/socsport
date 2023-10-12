d26f67b504a831f255edeb2e1d2a227371cba44b
MasterApp <40009220@parisnanterre.fr>
2023-10-12 13:32:17 +0200

    test3

--- src/App.js
+++ src/App.js
@@ -7,11 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-<<<<<<< HEAD
-          Edit <code>src/App.js</code> and save to reload. AMEL
-=======
           Edit <code>src/App.js</code> and save to reload. Alison 
->>>>>>> de379e5cbf4b6e24c3db14f0eea3a3a52e2bdc7b
         </p>
         <a
           className="App-link"
