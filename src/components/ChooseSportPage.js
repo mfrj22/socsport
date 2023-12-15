2e45c81ccf3d63e5c82d4104db7be381c38e621d
MasterApp <amelberrazouane2001@gmail.com>
2023-12-15 17:50:01 +0100

    récupérer les sports de la BD

--- src/components/ChooseSportPage.js
+++ src/components/ChooseSportPage.js
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ChooseSportForm from './ChooseSportForm';
 
 const ChooseSportPage = () => {
-  const recommendSport = (formData) => {
+  const [sports, setSports] = useState([]);
 
+  useEffect(() => {
+    // Effectuer une requête vers le backend pour obtenir la liste des sports
+    fetch('http://localhost:5000/sports')
+      .then(response => response.json())
+      .then(data => setSports(data))
+      .catch(error => console.error('Erreur lors de la récupération des sports :', error));
+  }, []);
+
+  const recommendSport = (formData) => {
     const { teamOrIndividual, typeSport, playStyle, indoorOutdoor } = formData;
 
     let recommendedSport = '';
 
     if (teamOrIndividual === 'equipe' && typeSport === 'ballon' && playStyle === 'pied') {
-      recommendedSport = 'football';
+        recommendedSport = sports.find(sport => sport.name.toLowerCase() === 'football');
     } else if (teamOrIndividual === 'equipe' && typeSport === 'ballon' && playStyle === 'main' && indoorOutdoor === 'interieur') {
-      recommendedSport = 'handball';
+        recommendedSport = sports.find(sport => sport.name.toLowerCase() === 'handball');
     } else if (teamOrIndividual === 'equipe' && typeSport === 'ballon' && playStyle === 'main' && indoorOutdoor === 'exterieur') {
-      recommendedSport = 'basketball';
+        recommendedSport = sports.find(sport => sport.name.toLowerCase() === 'basketball');
     } else if (teamOrIndividual === 'equipe' && typeSport === 'ballon' && playStyle === 'deux' && indoorOutdoor === 'exterieur') {
-      recommendedSport = 'rugby';
+        recommendedSport = sports.find(sport => sport.name.toLowerCase() === 'rugby');
     } else if (typeSport === 'raquette') {
-      recommendedSport = 'tennis';
+        recommendedSport = sports.find(sport => sport.name.toLowerCase() === 'tennis');
     } else if (typeSport === 'combat') {
-      recommendedSport = 'boxe';
+        recommendedSport = sports.find(sport => sport.name.toLowerCase() === 'boxe');
     } else {
-      recommendedSport = 'football';
+        recommendedSport = sports.find(sport => sport.name.toLowerCase() === 'football');
     }
 
     console.log('Sport recommandé :', recommendedSport);
