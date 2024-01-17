fbafcb5ff8288596f6811f963c14eb534b162d1d
MasterApp <40009220@parisnanterre.fr>
2024-01-17 12:10:31 +0100

    mise à jour front notif

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -1,48 +1,26 @@
 import React, { useEffect, useState } from 'react';
 
 function Notifications({ updateNotificationCount }) {
-  const [evenements, setEvenements] = useState([]);
-  // const [notificationCount, setNotificationCount] = useState(0);
+  const [notifications, setNotifications] = useState([]);
 
-  
   useEffect(() => {
-    // Récupérer les données nécessaires depuis le localStorage
-    const storedEvenements = JSON.parse(localStorage.getItem('evenements')) || [];
-    console.log('storedEvenements', storedEvenements);
-    // Filtrer les événements distincts et avec une date dans la semaine à venir
-    const filteredEvenements = filterEvenements(storedEvenements);
-    console.log('filteredEvenements', filteredEvenements);
-
-    // Mettre à jour le nombre de notifications
-    updateNotificationCount(filteredEvenements.length);
-    
-    setEvenements(filteredEvenements);
+    const username = localStorage.getItem('username');
+      fetch(`http://localhost:5000/notifications/${username}`, {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+      })
+      .then(response => response.json())
+      .then(data => {
+        setNotifications(data);
+        updateNotificationCount(notifications.length);
+        console.log('Success:', notifications.length);
+      })
+      .catch((error) => {
+          console.error('Error:', error);
+        });
   }, []);
-
-  // Fonction pour filtrer les événements distincts et avec une date dans la semaine à venir
-  const filterEvenements = (evenements) => {
-    const filtered = [];
-    const uniqueIds = new Set();
-
-    const currentDate = new Date();
-    const oneWeekFromNow = new Date();
-    oneWeekFromNow.setDate(currentDate.getDate() + 7);
-
-    evenements.forEach((evenement) => {
-      // Vérifier si l'événement a une date dans la semaine à venir
-      console.log('evenement.date', evenement.date)
-      const eventDate = new Date(evenement.date);
-      if (eventDate <= oneWeekFromNow && eventDate >= currentDate) {
-        // Vérifier si l'ID de l'événement est unique
-        if (!uniqueIds.has(evenement.id)) {
-          uniqueIds.add(evenement.id);
-          filtered.push(evenement);
-        }
-      }
-    });
-
-    return filtered;
-  };
   
 
 //   fonction pour calculer le nombre de jours restants
@@ -57,13 +35,13 @@ function Notifications({ updateNotificationCount }) {
   return (
     <div className="notifications-container">
       <h2>Notifications</h2>
-      {evenements.length > 0 ? (
+      {notifications.length > 0 ? (
         <ul>
-          {evenements.map((evenement) => (
-            <li key={evenement.id} className="notification-item">
-              L'événement {evenement.nom} a lieu dans {getDaysLeft(evenement.date)+1} jours.
+          {notifications.map((notification, index) => (
+            <li key={index} className="notification-item">
+              L'événement {notification.evenement_nom} a lieu dans {getDaysLeft(notification.evenement_date)+1} jours.
               <br />
-              {evenement.date} à {evenement.heure_debut}
+              {notification.evenement_date} à {notification.evenement_heure_debut}
             </li>
           ))}
         </ul>
