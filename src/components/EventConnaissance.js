921f5c30d9a93745803ab1af0222506ef01e77c4
MasterApp <40001083@parisnanterre.fr>
2024-01-18 16:43:15 +0100

    lien réservation event connaissance

--- src/components/EventConnaissance.js
+++ src/components/EventConnaissance.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function EventConnaissance() {
   const [events, setEvents] = useState([]);
+  const [reservations, setReservations] = useState([]);
   const username = localStorage.getItem('username');
 
   useEffect(() => {
@@ -9,6 +11,27 @@ function EventConnaissance() {
       .then(response => response.json())
       .then(data => setEvents(data));
   }, []);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/historique/${username}`)
+      .then(response => response.json())
+      .then(data => setReservations(data));
+  }, [username]);
+
+  const isEventReserved = (event) => {
+    return reservations.some(reservation => reservation.evenement_id === event.evenement_id);
+  };
+
+  const isEventPassed = (event) => {
+    const eventDateObj = new Date(event.evenement_date);
+    const currentDate = new Date();
+    return eventDateObj < currentDate;
+  };
+
+  const getReservationId = (event) => {
+    const reservation = reservations.find(reservation => reservation.evenement_id === event.evenement_id);
+    return reservation ? reservation.id : null;
+  };
 
   return (
     <table>
@@ -20,6 +43,7 @@ function EventConnaissance() {
           <th>Heure de fin</th>
           <th>Nom du terrain</th>
           <th>Connaissance</th>
+          <th>Réservation</th>
         </tr>
       </thead>
       <tbody>
@@ -31,7 +55,15 @@ function EventConnaissance() {
             <td>{event.evenement_heure_fin}</td>
             <td>{event.terrain_nom}</td>
             <td>{event.connaissance_nom}</td>
-         </tr>
+            <td>
+              {!isEventReserved(event) && !isEventPassed(event) && (
+                <Link to={`/add-reservation/${event.evenement_id}`}>Réserver</Link>
+              )}
+              {isEventReserved(event) && !isEventPassed(event) && (
+                <Link to={`/delete-reservation/${getReservationId(event)}`}>Annuler réservation</Link>
+              )}
+            </td>
+          </tr>
         ))}
       </tbody>
     </table>
