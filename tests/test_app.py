88d7d59c93df2d555c14d004c465ad7e7aa92c4b
MasterApp <40001083@parisnanterre.fr>
2024-01-20 11:58:34 +0100

    maj des tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,7 +5,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 import json
 import pytest
-from app import app, db, Terrain, Evenement
+from flask import Flask, jsonify
+from app import app, db, Evenement, Note, StatUser, User, Connaissance, Terrain, Ville, Reservation, Sport
 
 @pytest.fixture
 def client():
@@ -18,56 +19,200 @@ def client():
 
     yield client
 
-def test_nearest_fields(client):
-    # Tester la route /nearest-fields
-    response = client.post('/nearest-fields', json={'latitude': 48.906, 'longitude': 2.207})
+    # Nettoyage de la base de données après les tests
+    with app.app_context():
+        db.drop_all()
 
-    assert response.status_code == 200
-    data = response.get_json()
-
-    # Vérifier que les données renvoyées correspondent à nos attentes
-    assert data[0]['nom'] == 'Stade Vincent Pascucci'
-    assert data[1]['nom'] == 'Stade Gabriel Péri'
-    assert data[2]['nom'] == 'Stade Jean Moulin' 
+def test_login(client):
+    with app.app_context():
+        response = client.post('/login', json={'username': 'test_user'})
+        assert response.status_code == 200
+        assert response.json == {'message': 'Nouvel utilisateur créé'}
 
 def test_create_event(client):
-    # Tester la route /create-event/2
-    response = client.post('/create-event/2', json={
-        'name': 'Nom de l\'événement',
-        'date': '2023-11-01',
-        'startTime': '10:00:00',
-        'endTime': '12:00:00',
-        'nbParticipants': 10,
-    })
+    with app.app_context():
+        # Créer l'utilisateur seulement s'il n'existe pas déjà
+        if User.query.filter_by(username='test_user').first() is None:
+            user = User(username='test_user')
+            db.session.add(user)
+            db.session.commit()
 
+    # Créez un terrain pour le test
+        terrain = Terrain(nom='Test Terrain', latitude=0.0, longitude=0.0)
+        db.session.add(terrain)
+        db.session.commit()
+
+    # Test du point d'API /create-event/<int:fieldId>
+        response = client.post('/create-event/1', json={
+            'name': 'Test Event',
+            'date': '2024-01-20',
+            'startTime': '12:00',
+            'endTime': '14:00',
+            'nbParticipants': 10,
+            'username': 'test_user'
+        })
+
+        assert response.status_code == 200
+        assert response.json['message'] == 'Event created successfully'
+        assert 'terrain_name' in response.json
+
+def test_nearest_fields(client):
+    # Test du point d'API /nearest-fields
+    with app.app_context():
+        response = client.post('/nearest-fields', json={'latitude': 0.0, 'longitude': 0.0})
+        assert response.status_code == 200
+        assert isinstance(response.json, list)
+
+def test_add_reservation(client):
+    # Créez un utilisateur pour le test
+    with app.app_context():
+        # Créer l'utilisateur seulement s'il n'existe pas déjà
+        if User.query.filter_by(username='test_user').first() is None:
+            user = User(username='test_user')
+            db.session.add(user)
+            db.session.commit()
+
+        # Créez un terrain et un événement pour le test
+        terrain = Terrain(nom='Test Terrain', latitude=0.0, longitude=0.0)
+        db.session.add(terrain)
+        db.session.commit()
+
+        evenement = Evenement(
+            nom='Test Event',
+            date='2024-01-20',
+            heure_debut='12:00',
+            heure_fin='14:00',
+            nb_participants=10,
+            terrain_id=1,
+            username='test_user'
+        )
+        db.session.add(evenement)
+        db.session.commit()
+
+        # Test du point d'API /add-reservation
+        response = client.post('/add-reservation', json={
+            'evenement_id': 1,
+            'nom_participant': 'John',
+            'prenom_participant': 'Doe',
+            'email_participant': 'john.doe@example.com',
+            'tel_participant': '1234567890',
+            'username': 'test_user'
+        })
+
+        assert response.status_code == 200
+        assert response.json['message'] == 'Reservation created successfully'
+        assert 'evenement_id' in response.json
+        assert 'event_details' in response.json
+
+def test_add_note_to_event(client):
+    with app.app_context():
+        # Créez un utilisateur pour le test
+        user = User(username='test_user')
+        db.session.add(user)
+        db.session.commit()
+
+        # Créez un événement pour le test
+        evenement = Evenement(nom='Test Event', date='2024-01-20', heure_debut='12:00', heure_fin='14:00', username='test_user')
+        db.session.add(evenement)
+        db.session.commit()
+
+    # Test du point d'API /note-event/<int:eventId>
+    response = client.post('/note-event/1', json={'note': 4})
     assert response.status_code == 200
-    data = response.get_json()
+    assert response.json == {'message': 'Note added successfully'}
 
-def test_create_terrain(client):
-    # Tester la route /create-terrain
-    response = client.post('/ajouter-terrain', json={
-        'nom': 'Test Nom',
-        'adresse': 'Test Adresse',
-        'code_postal': 92000,
-        'departement': 'Test Département',
-        'latitude': 48,
-        'longitude': 2,
-        'horaire_ouverture': '10:00',
-        'horaire_fermeture': '12:00',
-        'ville': 'Ville du terrain',
-        'sport': [1, 2]
-    })
+def test_get_classement(client):
+    with app.app_context():
+        # Créez un utilisateur pour le test
+        user = User(username='test_user')
+        db.session.add(user)
+        db.session.commit()
 
+        # Créez des événements avec des notes
+        evenement1 = Evenement(nom='Event 1', username='test_user')
+        evenement2 = Evenement(nom='Event 2', username='test_user')
+        db.session.add_all([evenement1, evenement2])
+        db.session.commit()
+
+        note1 = Note(evenement_id=1, note=4)
+        note2 = Note(evenement_id=2, note=5)
+        db.session.add_all([note1, note2])
+        db.session.commit()
+
+    # Test du point d'API /classement-events
+    response = client.get('/classement-events')
     assert response.status_code == 200
-    data = response.get_json()
+    assert isinstance(response.json, list)
 
-def test_index_route(client):
-    # Tester la route '/'
-    response = client.get('/')
-    
+def test_sports(client):
+    # Envoi de la requête GET pour obtenir la liste des sports
+    response = client.get('/sports')
+
+    # Vérification de la réponse
     assert response.status_code == 200
-    data = response.get_json()
-    
-    # Vérifier que la réponse est une liste de terrains au format JSON
-    assert isinstance(data, list)
-    assert all('id' in terrain and 'nom' in terrain and 'adresse' in terrain and 'code_postal' in terrain and 'departement' in terrain and 'latitude' in terrain and 'longitude' in terrain and 'ville' in terrain for terrain in data)
+    assert isinstance(response.json, list)
+
+
+# Exemple de test pour la route /delete-reservation/<int:reservationId>
+def test_delete_reservation(client):
+    with app.app_context():
+        # Créez un utilisateur pour le test
+        user = User(username='test_user')
+        db.session.add(user)
+
+        ville = Ville(
+            nom="Nanterre", 
+            code_postal=92000, 
+            departement="Hauts-de-Seine"
+        )
+        db.session.add(ville)
+        db.session.commit()
+
+        sport = Sport(
+            name="Football"
+        )
+        db.session.add(sport)
+        db.session.commit()
+
+        terrain = Terrain(
+            nom='TestTerrain',
+            adresse='123 Rue du Test',
+            latitude=12.345,
+            longitude=67.890,
+            ville_id=ville.id,  # Utilisez l'ID de la ville créée
+            horaire_ouverture='08:00',
+            horaire_fermeture='18:00',
+            sports=[Sport.query.get(1)],
+        )
+        db.session.add(terrain)
+        db.session.commit()
+
+        event = Evenement(
+            nom='TestEvent',
+            date='2024-01-20',
+            heure_debut='12:00',
+            heure_fin='14:00',
+            nb_participants=10,
+            terrain_id=terrain.id,  # Utilisez l'ID du terrain créé
+            username='test_user',
+        )
+        db.session.add(event)
+        db.session.commit()
+
+        reservation = Reservation(
+            evenement_id=event.id,  # Utilisez l'ID de l'événement créé
+            nom_participant='John',
+            prenom_participant='Doe',
+            email_participant='john.doe@example.com',
+            tel_participant='1234567890',
+            username='test_user',
+        )
+        db.session.add(reservation)
+        db.session.commit()
+
+    # Envoi de la requête DELETE pour supprimer la réservation
+    response = client.delete('/delete-reservation/1')
+
+    # Vérification de la réponse
+    assert response.status_code == 200
+    assert response.json == {'message': 'Reservation deleted successfully'}
