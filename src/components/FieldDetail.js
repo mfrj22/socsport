ced2cd8ee856d7c1465848a01dd99cd806cd01de
Amel <amelberrazouane2001@gmail.com>
2024-02-19 13:52:23 +0100

    ajouter des points d'interets

--- src/components/FieldDetail.js
+++ src/components/FieldDetail.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L from 'leaflet';
 import { useParams } from 'react-router-dom';
 
 const FieldDetail = () => {
@@ -8,30 +6,39 @@ const FieldDetail = () => {
   const [field, setField] = useState(null);
   const [weather, setWeather] = useState(null);
   const [pointsOfInterest, setPointsOfInterest] = useState([]);
-
-  const options = {
-    method: 'GET',
-    headers: {accept: 'application/json'}
-  };
-
-  const clientId = 'LKTDWVWWHKOOT5UOGJQD0QIP1DDMD0D1FFBQPWV0TXO2IYFW';
-  const clientSecret = 'FPEE3SXTUEQOEPEO3PICQN1RPN5VLE14JMURXS0O4CXLTCHP';
+  
   useEffect(() => {
     const fetchData = async () => {
-      // Récupérer les informations du terrain
-      const responseField = await fetch(`http://localhost:5000/fields/${fieldId}`);
-      const dataField = await responseField.json();
-      setField(dataField);
-
-      // Récupérer la météo
-      const responseWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${dataField.latitude}&lon=${dataField.longitude}&appid=e46c347779f1d83501e8fda216cf14e1`);
-      const dataWeather = await responseWeather.json();
-      setWeather(dataWeather);
-
-      // Récupérer les points d'intérêt
-      const responsePOI = await fetch(`https://api.foursquare.com/v3/places/search?client_id=${clientId}&client_secret=${clientSecret}&ll=${dataField.latitude},${dataField.longitude}`, options);
-      const dataPOI = await responsePOI.json();
-      setPointsOfInterest(dataPOI.response.venues);
+      try {
+        // Récupérer les informations du terrain
+        const responseField = await fetch(`http://localhost:5000/fields/${fieldId}`);
+        const dataField = await responseField.json();
+        setField(dataField);
+        
+        // Récupérer la météo
+        const responseWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${dataField.latitude}&lon=${dataField.longitude}&appid=e46c347779f1d83501e8fda216cf14e1`);
+        const dataWeather = await responseWeather.json();
+        setWeather(dataWeather);
+        
+        // Récupérer les points d'intérêt depuis l'API Foursquare
+        const options = {
+          method: 'GET',
+          headers: {
+            accept: 'application/json',
+            Authorization: 'fsq3v0NQyvk7WzGQR2Au4aTz5YcvcOw9WNci7Th+6nxDI9g='
+          }
+        };
+        const responsePOI = await fetch(`https://api.foursquare.com/v3/places/search?ll=${dataField.latitude},${dataField.longitude}`, options);
+        const dataPOI = await responsePOI.json();
+        
+        if (dataPOI.results) {
+          setPointsOfInterest(dataPOI.results);
+        } else {
+          console.error('Réponse invalide de l\'API Foursquare :', dataPOI);
+        }
+      } catch (error) {
+        console.error('Erreur lors de la récupération des données :', error);
+      }
     };
 
     fetchData();
@@ -47,22 +54,13 @@ const FieldDetail = () => {
       <p>Latitude: {field.latitude}</p>
       <p>Longitude: {field.longitude}</p>
       <img src={`https://openweathermap.org/img/w/${weather.weather[0].icon}.png`} alt="Weather Icon" style={{ marginRight: '5px' }} />
-      {weather.name} : {Math.round(weather.main.temp - 273.15)}°C
-
-      <MapContainer center={[field.latitude, field.longitude]} zoom={13}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        />
-        <Marker position={[field.latitude, field.longitude]}>
-          <Popup>{field.name}</Popup>
-        </Marker>
-        {pointsOfInterest && pointsOfInterest.map((poi) => (
-          <Marker position={[poi.location.lat, poi.location.lng]} key={poi.id}>
-            <Popup>{poi.name}</Popup>
-          </Marker>
+      {weather.name} : {Math.round(weather.main.temp - 273.15)}°C <br/>
+      <h2>Points d'intérêt à proximité :</h2>
+      <ul>
+        {pointsOfInterest && pointsOfInterest.length > 0 && pointsOfInterest.map((poi) => (
+          <li key={poi.id}>{poi.name}</li>
         ))}
-      </MapContainer>
+      </ul>
     </div>
   );
 };
